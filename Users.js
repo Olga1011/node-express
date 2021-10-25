fbab5ea1aeed353527ebc06bf80d41fbfca48895
Olga1011 <56080331+Olga1011@users.noreply.github.com>
2021-10-25 18:21:37 +0300

    Added CRUD requests

---
+++ Users.js
@@ -0,0 +1,23 @@
+let users = [
+  { id: 1, name: "John Smith", age: 25, job: "programmer" },
+  {
+    id: 2,
+    name: "Amanda Sunny",
+    age: 28,
+    job: "administrative assistant",
+  },
+  {
+    id: 3,
+    name: "Egor Cleaven",
+    age: 20,
+    job: "administrator",
+  },
+  {
+    id: 4,
+    name: "Samantha David",
+    age: 34,
+    job: "system administrator",
+  },
+];
+
+module.exports = users;
--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 const express = require("express");
+const users = require("./Users");
+
 const path = require("path");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//get all users
+
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+//get one user
+
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("The user was not found");
+  res.send(user);
+});
+
+//create a user
+app.post("/api/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    age: req.body.age,
+    job: req.body.job,
+  };
+  if (!newUser.name || !newUser.age || !newUser.age || !newUser.job)
+    return res.status(400).send("Please include the name and the age");
+  users.push(newUser);
+  res.send(users);
+});
+
+//update a user
+app.put("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) res.status(404).send("The user was not found");
+
+  const updateUser = req.body;
+
+  if (user.id === parseInt(req.params.id)) {
+    user.name = updateUser.name;
+    user.age = updateUser.age;
+    user.job = updateUser.job;
+
+    res.send(users);
+  }
+});
+
+//delete a user
+app.delete("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("The user was not found");
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+  res.send(user);
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
